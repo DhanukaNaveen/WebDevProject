72cb033d52f8b7c111ebb742dc2e5f45381e729a
Dhanuka <dhanuka200098@gmail.com>
2025-09-06 11:14:54 +0530

    Day 22-2

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,17 +1,13 @@
-import express from "express";
-import { createProduct } from "../controllers/productController.js"; 
-import { getProducts } from "../controllers/productController.js";
-import { deleteProduct } from "../controllers/productController.js";
-import { updateProduct } from "../controllers/productController.js";
-import { getProductInfo } from "../controllers/productController.js";
-const productRouter = express.Router();
+import express from 'express';
+import { createProduct, deleteProduct, getProductInfo, getProducts, searchProducts, updateProduct } from '../controllers/productController.js';
 
-// POST route for creating a product
-productRouter.post("/", createProduct);
-productRouter.get("/", getProducts);
-productRouter.delete("/:productId", deleteProduct);
-productRouter.put("/:productId", updateProduct);
-productRouter.get("/:productId", getProductInfo);
+
+const productRouter = express.Router();
+productRouter.post("/",createProduct)
+productRouter.get("/",getProducts)
+productRouter.get("/:productId", getProductInfo) 
+productRouter.delete("/:productId", deleteProduct)
+productRouter.put("/:productId", updateProduct)
 productRouter.get("/search/:query", searchProducts)
 
 export default productRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,15 +1,13 @@
-import express from "express";
-
-import { createUser, loginUser } from "../controllers/userController.js";
-import { get } from "mongoose";
+import express from 'express';
+import { createUser, getUser, googleLogin, loginUser, resetPassword, sendOTP } from '../controllers/userController.js';
 
 const userRouter = express.Router();
-
 userRouter.post("/",createUser)
+userRouter.get("/",getUser)
 userRouter.post("/login",loginUser)
-userRouter.get("/",getUser)//get user info by token
 userRouter.post("/google-login", googleLogin)
 userRouter.post("/send-otp", sendOTP)
 userRouter.post("/reset-password",resetPassword)
 
-export default userRouter
+
+export default userRouter;
