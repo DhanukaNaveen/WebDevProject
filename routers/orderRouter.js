92fa8de668d3d44db12929d2d16713dc611b796f
Dhanuka <dhanuka200098@gmail.com>
2025-11-01 23:53:56 +0530

    updated

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { createOrder, getOrders, updateOrder, getAllOrders } from '../controllers/orderController.js';
+import { createOrder, getOrders, updateOrder, getAllOrders} from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 orderRouter.post("/", createOrder)
 orderRouter.get("/", getAllOrders);
 orderRouter.put("/:orderId", updateOrder) 
-router.get("/:page/:limit", getOrders);
+orderRouter.get("/:page/:limit", getOrders);
 
 
 export default orderRouter;
